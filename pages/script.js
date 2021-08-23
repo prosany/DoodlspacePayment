26b053753ca36e5d064f6be3a9fd7266e4830452
Mahabub Sany <mahabubsany2@gmail.com>
2021-08-23 16:56:45 +0600

    fix

--- pages/script.js
+++
@@ -1,9 +0,0 @@
-document.querySelector("#orderId").innerHTML = `#${Math.floor(100000 + Math.random() * 900434343000)}`;
-
-
-document.querySelector("#advPack").innerText = `${localStorage.getItem("advPack")}`;
-document.querySelector("#advPrice").innerText = `${localStorage.getItem("advPrice")}`;
-
-
-document.querySelector("#stdPack").innerText = `${localStorage.getItem("stdPack")}`;
-document.querySelector("#stdPrice").innerText = `${localStorage.getItem("stdPrice")}`;
